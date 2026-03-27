5324046fc3eeb729ef5ccc743fb11b60cf3fc6c2
mongodbramesh-sudo <mongodb.ramesh@gmail.com>
2026-03-27 11:41:27 +0530

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World from Git!"
+    return "Hello World from Git repo!"
 
 if __name__ == "__main__":
     app.run(debug=True)
